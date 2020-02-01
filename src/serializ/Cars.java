5d3379916f717d9751f9448e55dc4b0be3a37e6c
FlyLord8902 <56519231+FlyLord8902@users.noreply.github.com>
2020-02-01 18:21:01 -0500

    updating

--- src/serializ/Cars.java
+++ src/serializ/Cars.java
@@ -1,5 +1,54 @@
 package serializ;
 
-public class Cars {
-    String hello;
+import java.io.*;
+
+import java.math.BigDecimal;
+
+public class Cars implements Serializable {
+    private String brand;
+    private BigDecimal price;
+
+    public Cars(String brand, BigDecimal price){
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public  void serializeCar(Cars car){
+        try{
+            FileOutputStream fileOut = new FileOutputStream("myCars.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+            out.writeObject(car);
+            out.close();
+            fileOut.close();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public  Cars deSerializeCar(){
+        try {
+            FileInputStream fileIn = new FileInputStream("myCars.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+             Cars newCar = (Cars)in.readObject();
+
+             fileIn.close();
+             in.close();
+             return newCar;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public boolean equals(Object other ){
+        if(!this.brand.equals(((Cars) other).brand) || !this.price.equals(((Cars)other).price)){
+            return false;
+        }
+        return true;
+
+    }
 }
--- src/serializ/Driver.java
+++ src/serializ/Driver.java
@@ -1,4 +1,23 @@
 package serializ;
 
+import java.math.BigDecimal;
+
 public class Driver {
+    public static void main(String[]args){
+        Cars car1 = new Cars("Lamborghini",new BigDecimal(417826));
+        try {
+            car1.serializeCar(car1);
+            System.out.println("Car has been serialized...");
+            Cars car2 = car1.deSerializeCar();
+            System.out.println("Car has been de-serialized from file...");
+            System.out.println();
+
+            if(car2.equals(car1)){
+                System.out.println("Car has been successfully de-serialized and are equal");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
